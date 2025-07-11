1267b5d7f9abc5c908cdeb6dddf80afcf551ac0b
Adesola <dessyjoy111@gmail.com>
2025-07-11 03:38:00 +0200

    another commit

--- Backend/tasks/views.py
+++ Backend/tasks/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+class TaskViewset(viewsets.ModelViewSet):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = TaskSerializer
+    queryset = Task.objects.all()
