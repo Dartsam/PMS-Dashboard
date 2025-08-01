ddf295ecc31265c9867426b4104b4b32a665b677
unknown <nsinkanga@gmail.com>
2025-08-01 12:00:59 +0100

    NominalRoll.jsx

--- frontend/src/router/index.jsx
+++ frontend/src/router/index.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from '../auth/LoginPage';
 import AppLayout from '@/layouts/AppLayout';
 import DashboardPage from '@/features/dashboard/DashboardPage';
+import NominalRoll from '@/features/staff/nominalRoll/NominalRoll';
+
 
 function AppRouter() {
   return (
@@ -11,6 +13,8 @@ function AppRouter() {
         <Route path="/login" element={<LoginPage />} />
         <Route element={<AppLayout />}>
           <Route index element={<DashboardPage />} />
+          <Route path="staff/nominal-roll" element={<NominalRoll />} />
+
         </Route>
       </Routes>
     </BrowserRouter>
